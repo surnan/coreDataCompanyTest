005fc3951a589025bb21c2cd061d6a9b2e32ff1c
surnan <forsuresh@yandex.com>
2018-02-10 07:02:23 -0500

    code cleanup.  Deleted bunch of unnecessary comments

--- coreDataCompanyTest/coreDataCompanyTest/CompaniesController.swift
+++ coreDataCompanyTest/coreDataCompanyTest/CompaniesController.swift
@@ -26,55 +26,34 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
     ]
     
     func addCompany(company: Company) {
-//        let tesla = Company(name: "Tesla", founded: Date())
-//        companies.append(tesla)
-        
         let newCompany = Company(name: company.name, founded: Date())
         companies.append(newCompany)
-        
-        
         let newIndexPath = IndexPath(row: companies.count - 1, section: 0)
         tableView.insertRows(at: [newIndexPath], with: .automatic)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "TEST", style: .plain, target: self, action: #selector(addCompany))
-        
-        
-        
-        
-        
-        view.backgroundColor = .white
+       view.backgroundColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus") .withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
         navigationItem.title = "Companies"
         
-//        setupNavigationStyle()
         tableView.backgroundColor = UIColor.darkBlue
-//        tableView.separatorStyle = .none
         tableView.separatorColor = UIColor.white
-        tableView.tableFooterView = UIView()  // if you set your footer to blank, you won't get mulitple separator lines after final row
+        tableView.tableFooterView = UIView()  // footer = blank, won't get mulitple separator lines after final row
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
     }
     
     @objc func handleAddCompany() {
-        print("Add button pressed")
-        
         let createCompanyController = CreateCompanyController()
         let navController = CustomNavigationController(rootViewController: createCompanyController)
-        
-//        createCompanyController.companiesController = self
         createCompanyController.delegate = self
-        
         present(navController, animated: true, completion: nil)
     }
     
     func setupNavigationStyle() {
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.barTintColor = UIColor.lightRed
-        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
