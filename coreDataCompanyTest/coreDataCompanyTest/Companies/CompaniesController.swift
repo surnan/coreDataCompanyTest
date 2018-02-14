f52b6154503cf6d010c1dbf31ce1de7d83d5304c
surnan <forsuresh@yandex.com>
2018-02-13 21:03:54 -0500

    custom uitableviewcell implemented.  Moved a lot of the code from view controller into separate file that contains uitableview class

--- coreDataCompanyTest/coreDataCompanyTest/Companies/CompaniesController.swift
+++ coreDataCompanyTest/coreDataCompanyTest/Companies/CompaniesController.swift
@@ -52,7 +52,12 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
         tableView.backgroundColor = UIColor.darkBlue
         tableView.separatorColor = UIColor.white
         tableView.tableFooterView = UIView()  // footer = blank, won't get mulitple separator lines after final row
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+//
+        tableView.register(CompanyCell.self, forCellReuseIdentifier: "cellID")  //with this line alone (nothing changes)
+        
+        
     }
     
     @objc private func handleReset(){
@@ -106,32 +111,19 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
-        cell.backgroundColor = UIColor.teal
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as! CompanyCell
         let company = companies[indexPath.row]
-        if let name = company.name, let founded = company.founded {
-            // One way to show date
-            //            let locale = Locale(identifier: "EN")
-            //            let dateString = "\(name) - Founded: \(founded.description(with: locale))"
-            //MMM dd, YYYY
-            let dateFormatter = DateFormatter()
-            //            dateFormatter.dateFormat = "MMM dd, yyyy hh:mm:ss" // <-- Will show hour, minute, seconds also
-            dateFormatter.dateFormat = "MMM dd, yyyy"  //first 3 letters of month date, 4-digit year
-            let foundedDateString = dateFormatter.string(from: founded)
-            let dateString = "\(name) - Founded: \(foundedDateString)"
-            cell.textLabel?.text = dateString
-        } else {
-            cell.textLabel?.text = company.name
-        }
-        //        cell.textLabel?.text = "\(company.name) - Founded: \(company.founded)"
-        cell.textLabel?.textColor = UIColor.white
-        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        cell.imageView?.image = #imageLiteral(resourceName: "select_photo_empty")
-        if let imageData = company.imageData {
-            cell.imageView?.image = UIImage(data: imageData)
-        }
+        cell.company = company
         return cell
     }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let view = UIView()
---
+++ coreDataCompanyTest/coreDataCompanyTest/Companies/CompanyCell.swift
@@ -0,0 +1,83 @@
+//
+//  CompanyCell.swift
+//  coreDataCompanyTest
+//
+//  Created by admin on 2/13/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class CompanyCell: UITableViewCell {
+    
+    var company: Company? {  //triggered from 'cellForRowAt' cell.company=company
+        didSet {
+            print(company?.name)
+            
+            if let imageData = company?.imageData {
+                companyImageView.image = UIImage(data: imageData)
+            }
+            nameFoundedDateLabel.text = company?.name
+            
+            if let name = company?.name, let founded = company?.founded {
+                // One way to show date
+                //            let locale = Locale(identifier: "EN")
+                //            let dateString = "\(name) - Founded: \(founded.description(with: locale))"
+                //MMM dd, YYYY
+                let dateFormatter = DateFormatter()
+                //            dateFormatter.dateFormat = "MMM dd, yyyy hh:mm:ss" // <-- Will show hour, minute, seconds also
+                dateFormatter.dateFormat = "MMM dd, yyyy"  //first 3 letters of month date, 4-digit year
+                let foundedDateString = dateFormatter.string(from: founded)
+                let dateString = "\(name) - Founded: \(foundedDateString)"
+                nameFoundedDateLabel.text = dateString
+            } else {
+                nameFoundedDateLabel.text = company?.name
+            }
+            
+        }
+    }
+    
+    //custom image view
+    let companyImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "select_photo_empty"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.darkBlue.cgColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let nameFoundedDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "COMPANY NAME"
+        label.textColor = UIColor.white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = UIColor.teal
+        
+        addSubview(companyImageView)
+        companyImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        companyImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        companyImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        companyImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        addSubview(nameFoundedDateLabel)
+        nameFoundedDateLabel.leftAnchor.constraint(equalTo: companyImageView.rightAnchor, constant: 8).isActive = true
+        nameFoundedDateLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        nameFoundedDateLabel.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        nameFoundedDateLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
